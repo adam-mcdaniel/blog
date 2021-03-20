679898deec36422b79ca2edd3ccf94cec2304bba
Adam McDaniel <adam.mcdaniel17@gmail.com>
2021-03-20 02:44:00 -0400

    Used NPM to deploy

--- config.js
+++ config.js
@@ -1,45 +1,44 @@
 'use strict';
 
 module.exports = {
-  url: 'https://adam-mcdaniel.github.io',
-  pathPrefix: '/blog/',
-  title: 'Adam | Blog',
-  subtitle: 'Discussions about my projects and computer science in general.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-  ],
-  author: {
-    name: 'Adam McDaniel',
-    photo: '/photo.png',
-    bio: 'Programmer, musician, and student at UTK.',
-    contacts: {
-      email: 'adam.mcdaniel17@gmail.com',
-      facebook: '',
-      telegram: '',
-      twitter: 'adamthekiwi99',
-      github: 'adam-mcdaniel',
-      rss: '',
-      vkontakte: '',
-      linkedin: 'adam-mcdaniel-7973a6192/',
-      instagram: 'adamthekiwi16',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
+    url: 'https://adam-mcdaniel.github.io',
+    pathPrefix: '/blog/',
+    title: 'Adam | Blog',
+    subtitle: 'Discussions about my projects and computer science in general.',
+    copyright: '© All rights reserved.',
+    disqusShortname: '',
+    postsPerPage: 4,
+    googleAnalyticsId: 'UA-73379983-2',
+    useKatex: false,
+    menu: [{
+            label: 'Articles',
+            path: '/'
+        },
+        {
+            label: 'About me',
+            path: '/pages/about'
+        },
+    ],
+    author: {
+        name: 'Adam McDaniel',
+        photo: '/photo.png',
+        bio: 'Programmer, musician, and student at UTK.',
+        contacts: {
+            email: 'adam.mcdaniel17@gmail.com',
+            facebook: '',
+            telegram: '',
+            twitter: 'adamthekiwi99',
+            github: 'adam-mcdaniel',
+            rss: '',
+            vkontakte: '',
+            linkedin: 'adam-mcdaniel-7973a6192/',
+            instagram: 'adamthekiwi16',
+            line: '',
+            gitlab: '',
+            weibo: '',
+            codepen: '',
+            youtube: '',
+            soundcloud: '',
+        }
     }
-  }
 };
